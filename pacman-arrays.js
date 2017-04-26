8c3e3c90d42a24e7d355c8726937f29cd2bd76cf
ilia24 <iliademertchian24@gmail.com>
2017-04-26 01:16:10 -0400

    create pacman arrays file with code

---
+++ pacman-arrays.js
@@ -0,0 +1,8 @@
+var ghosts = ['inky', 'blinky', 'pinky', 'clyde'];
+
+console.log('there are ' + ghosts.length + ' ghosts')
+console.log('their names are:')
+console.log(ghosts[0]);
+console.log(ghosts[1]);
+console.log(ghosts[2]);
+console.log(ghosts[3]);
